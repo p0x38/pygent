c78c58b315cdfc883ddad098d1876e313a6ecebb
p0x38 <ryuuking0410@gmail.com>
2026-09-03 22:53:36 +0900

    feat(syntax): add extensible syntax API

---
+++ src/pygent/syntax/__init__.py
@@ -0,0 +1,11 @@
+"""Extensible conversational syntax for Pygent."""
+
+from pygent.syntax.base import SyntaxContext, SyntaxHandler, SyntaxResult
+from pygent.syntax.registry import SyntaxRegistry
+
+__all__ = [
+    "SyntaxContext",
+    "SyntaxHandler",
+    "SyntaxRegistry",
+    "SyntaxResult",
+]
