0adfe5168efe7fa6ba868bdcb7925295876e7d97
p0x38 <ryuuking0410@gmail.com>
2026-09-03 23:34:21 +0900

    feat(syntax): export plugin discovery helpers

--- src/pygent/syntax/__init__.py
+++ src/pygent/syntax/__init__.py
@@ -1,11 +1,17 @@
 """Extensible conversational syntax for Pygent."""
 
 from pygent.syntax.base import SyntaxContext, SyntaxHandler, SyntaxResult
+from pygent.syntax.discovery import (
+    ENTRY_POINT_GROUP,
+    discover_syntax_plugins,
+    load_syntax_plugins,
+)
 from pygent.syntax.parser import ParsedInput, SyntaxInvocation, SyntaxParser
 from pygent.syntax.plugins import SyntaxPlugin, inject_syntax_plugin
 from pygent.syntax.registry import SyntaxRegistry
 
 __all__ = [
+    "ENTRY_POINT_GROUP",
     "ParsedInput",
     "SyntaxContext",
     "SyntaxHandler",
@@ -14,5 +20,7 @@ __all__ = [
     "SyntaxPlugin",
     "SyntaxRegistry",
     "SyntaxResult",
+    "discover_syntax_plugins",
     "inject_syntax_plugin",
+    "load_syntax_plugins",
 ]
