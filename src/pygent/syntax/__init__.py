27e4b6057bae8aadfe4df559d605d824f6fa446c
p0x38 <ryuuking0410@gmail.com>
2026-09-03 23:29:05 +0900

    feat(syntax): export built-in syntax plugins

--- src/pygent/syntax/__init__.py
+++ src/pygent/syntax/__init__.py
@@ -1,11 +1,20 @@
 """Extensible conversational syntax for Pygent."""
 
 from pygent.syntax.base import SyntaxContext, SyntaxHandler, SyntaxResult
+from pygent.syntax.builtin import (
+    BuiltinSyntaxPlugin,
+    CommandHandler,
+    MentionHandler,
+    create_builtin_syntax_registry,
+)
 from pygent.syntax.parser import ParsedInput, SyntaxInvocation, SyntaxParser
 from pygent.syntax.plugins import SyntaxPlugin, inject_syntax_plugin
 from pygent.syntax.registry import SyntaxRegistry
 
 __all__ = [
+    "BuiltinSyntaxPlugin",
+    "CommandHandler",
+    "MentionHandler",
     "ParsedInput",
     "SyntaxContext",
     "SyntaxHandler",
@@ -14,5 +23,6 @@ __all__ = [
     "SyntaxPlugin",
     "SyntaxRegistry",
     "SyntaxResult",
+    "create_builtin_syntax_registry",
     "inject_syntax_plugin",
 ]
