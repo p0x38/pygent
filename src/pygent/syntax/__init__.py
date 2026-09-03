ec042f7a97193b3a881e097045296ae76e4cb8d5
p0x38 <ryuuking0410@gmail.com>
2026-09-03 22:54:25 +0900

    feat(syntax): expose parser and plugin APIs

--- src/pygent/syntax/__init__.py
+++ src/pygent/syntax/__init__.py
@@ -1,11 +1,18 @@
 """Extensible conversational syntax for Pygent."""
 
 from pygent.syntax.base import SyntaxContext, SyntaxHandler, SyntaxResult
+from pygent.syntax.parser import ParsedInput, SyntaxInvocation, SyntaxParser
+from pygent.syntax.plugins import SyntaxPlugin, inject_syntax_plugin
 from pygent.syntax.registry import SyntaxRegistry
 
 __all__ = [
+    "ParsedInput",
     "SyntaxContext",
     "SyntaxHandler",
+    "SyntaxInvocation",
+    "SyntaxParser",
+    "SyntaxPlugin",
     "SyntaxRegistry",
     "SyntaxResult",
+    "inject_syntax_plugin",
 ]
