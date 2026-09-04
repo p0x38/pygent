4093e6461fa35ebca016f6c5d0950e88ff94de45
p0x38 <ryuuking0410@gmail.com>
2026-09-04 21:05:55 +0900

    feat(search): export DuckDuckGo provider

--- src/pygent/search/__init__.py
+++ src/pygent/search/__init__.py
@@ -1,5 +1,6 @@
 """Provider-neutral web search abstractions."""
 
 from pygent.search.base import SearchProvider, SearchResult
+from pygent.search.duckduckgo import DuckDuckGoSearch
 
-__all__ = ["SearchProvider", "SearchResult"]
+__all__ = ["DuckDuckGoSearch", "SearchProvider", "SearchResult"]
